4ccdb3fd1899f134783a7bef3526e15830770463
Dario <fakkoweb@libero.it>
2019-02-03 13:21:02 +0100

    Added Unreal project with empty Test map... time to go to work

---
+++ Source/UFurnitureResize/UFurnitureResizeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UFurnitureResizeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UFURNITURERESIZE_API AUFurnitureResizeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
